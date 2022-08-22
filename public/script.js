7f511f3da3634cdf49fe98f4c70092e01c6f0264
Guillermo Pereira <65512274+Ferbit2001@users.noreply.github.com>
2022-08-22 18:30:17 +0100

    Delete public directory

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const datos = async () => {
-    return fetch('/v1/ingredient/').then(res => res.json())
-}
-const nav = document.getElementById('nav');
-nav.addEventListener('click', e => (e.target.innerText == 'Recetas') ? decks[e.target.innerText]() : (e.target.innerText == 'Precios') ? decks[e.target.innerText]() : null);
-
