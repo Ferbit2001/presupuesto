9f1dd82470f8be118865a749510b9408ec0bcbfc
Ferbit2001 <guillermopereira7@gmail.com>
2022-09-06 19:33:36 +0100

    'finish'

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-// set api routes
-
-require('./src/routes/api')(app)
-
 app.use(cors())
 app.use(express.json())
+
+require('./src/routes/api')(app)
 
 app.listen(80,"0.0.0.0",()=>console.log('*****Server en linea*****'))
