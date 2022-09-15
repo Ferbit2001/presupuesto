5ca84a200e87f56026f25d5694ddcb114d998d21
Ferbit2001 <guillermopereira7@gmail.com>
2022-09-15 04:09:04 +0100

    finish

--- models/price.js
+++ models/price.js
@@ -2,6 +2,7 @@ const {model, Schema} = require('mongoose')
 
 const PriceSchema = new Schema({
     name: {type: String, required: true},
+    pname: {type:String, required:true},
     price: {type: Number, required: false},
     shop: {type: String, required: false},
     price_per_unit: {type: Number, required: false},
